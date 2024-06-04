d70595b4076a079d8581e12178a49339af92a9e9
bfeitknecht <bfeitknecht@ethz.ch>
2024-06-04 19:50:02 +0200

    chore(fish): modified:   conf.d/fish_user_key_bindings.fish

--- .config/fish/conf.d/fish_user_key_bindings.fish
+++ .config/fish/conf.d/fish_user_key_bindings.fish
@@ -1,13 +1,14 @@
 function fish_user_key_bindings --description "Define default user key bindings"
 
-    bind \r execute_trans
+    bind \r trans-execute
     bind \e- cdup
-    bind \el clear-screen # --status
+    bind \ek clear-screen
+    bind \el clear-ls
     bind \cE end-of-line
 
 end
 
-function execute_trans
+function trans-execute
     if commandline --is-valid
     # or test (count $argv) -eq 0
         set -g TRANSIENT 1
@@ -16,4 +17,12 @@ function execute_trans
         set -g TRANSIENT 0
     end
     commandline -f execute
+end
+
+function clear-ls
+    echo
+    clear
+    ls
+    echo
+    commandline -f repaint
 end
