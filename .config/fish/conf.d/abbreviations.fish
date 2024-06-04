9afcfa78371b1dce790c49a58a47ae9bab05151c
bfeitknecht <bfeitknecht@ethz.ch>
2024-06-04 20:18:09 +0200

    chore(fish): modify abbreviations

--- .config/fish/conf.d/abbreviations.fish
+++ .config/fish/conf.d/abbreviations.fish
@@ -1,20 +1,15 @@
+# misc
 abbr :q exit
 abbr :e micro
-
 abbr brcl brew cleanup --prune all
-abbr brug brew upgrade
-abbr brud brew update
-abbr bris brew install
-abbr brif brew info
-abbr brrm brew remove
-abbr brse brew search
 
+# git related
 abbr gco git checkout
 abbr gc git commit
+abbr gca git commit --amend
 abbr ga git add .
 abbr gs git status -sb
 abbr gp git push --verbose
 
-# abbr rm trash -F
-
+# extra functions
 abbr ev echo-variable
--- .config/fish/conf.d/fish_user_key_bindings.fish
+++ .config/fish/conf.d/fish_user_key_bindings.fish
@@ -3,7 +3,9 @@ function fish_user_key_bindings --description "Define default user key bindings"
     bind \r trans-execute
     bind \e- cdup
     bind \ek clear-screen
-    bind \el clear-ls
+    # bind \el clear-ls
+    # bind \el 'commandline -f clear-screen; __fish_list_current_token'
+    bind \el '__fish_list_current_token'
     bind \cE end-of-line
 
 end
@@ -20,9 +22,15 @@ function trans-execute
 end
 
 function clear-ls
-    echo
+    # echo
+    # commandline -f clear-screen
+    # ls
+    # echo
+    
     clear
     ls
     echo
     commandline -f repaint
+    # commandline -f cancel
+    # commandline -f execute
 end
