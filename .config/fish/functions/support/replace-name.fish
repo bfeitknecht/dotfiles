f31365d9c3be45d89049b98caaa6572809abd3c5
bfeitknecht <bfeitknecht@ethz.ch>
2024-09-19 14:04:03 +0200

    feat(fish): update fisher, work on replace-name
    new file:   replace-name.fish
    modified:   ../../plugins/functions/fisher.fish

---
+++ .config/fish/functions/support/replace-name.fish
@@ -0,0 +1,31 @@
+function replace-name --description "replace-name <find> <replace> [PATH] [OPTIONS]"
+  
+  echo $argv | read -l find replace
+  
+  argparse \
+    'f/force' \
+    'r/recurse' \
+    -- $argv
+  or set arg_err
+ 
+  # at this point find and replace are still in $argv
+  #echo $argv
+  
+  echo $argv | read -l path
+  if test -n "$path"
+    cd -- $path     # if path given go there
+  end
+  
+  #set -q _flag_recurse
+  #and set files (fd)
+  #or set files (fd -d 1)  # only top level files
+  
+  set files (fd)
+
+  for f in $files
+    set -l new (string replace $find $replace $f)
+    mv $f $new
+  end
+
+end
+
